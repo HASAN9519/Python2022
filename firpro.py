3fa4aba5b569df0d5855674280375e84e37889d7
HASAN9519 <shehab10111995@gmail.com>
2022-08-08 19:55:14 +0600

    new

--- firpro.py
+++ firpro.py
@@ -1,6 +1,7 @@
 fname = input("enter first name: ")
+mname = input("enter middle name: ")
 lname = input("enter last name: ")
-fullname = fname+"@"+lname
+fullname = fname+"."+mname+"."+lname+"@gmail.com"
 
 print("Full Name:", fullname)
 print("he is a bangladeshi")
