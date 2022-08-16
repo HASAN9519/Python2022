afec6014ecc4dbd1401db3304651bb1ecbed5a49
HASAN9519 <shehab10111995@gmail.com>
2022-08-16 19:46:11 +0600

    new

--- new.py
+++ new.py
@@ -1,5 +1,14 @@
-print("hello world")
+#print("hello world")
 
-print('let the battle begin')
+#print('let the battle begin')
 
-print("dragon")
+#print("dragon")
+#exception will only occure when b will be 0
+a = 1
+
+try:
+    b = int(input("Please enter a number to divide a: "))
+    a = a/b
+    print("Success a = ",a)
+except:
+    print("There was an error")
